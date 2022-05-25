0f6b31d6cb9a6485b8fa7b6f0b081f055b49cd6d
Guillaume Leclerc <guill.bout@gmail.com>
2022-05-25 20:50:53 +0800

    Initial Commit

---
+++ ListsDrop/ListsDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsDropApp.swift
+//  ListsDrop
+//
+//  Created by Guillaume Leclerc on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListsDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
